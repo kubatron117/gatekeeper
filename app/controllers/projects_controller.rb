ebba4b7befb48014b6e5e26f23a67ee5b5bcedc3
Kubatron 117 <kubatron117@gmail.com>
2025-06-20 16:52:31 +0200

    WIP - show tasks for project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,6 +8,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1 or /projects/1.json
   def show
+    @tasks = Task.where(project_id: @project.id)
   end
 
   # GET /projects/new
