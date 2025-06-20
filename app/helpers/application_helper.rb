38a476049e99bccc4b4b645dfa356c4696ac1501
Zdeněk Carbol <zdenek.carbol.711@gmail.com>
2025-06-20 16:40:56 +0200

    Add navbar and add styles to edit user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_link_to(name, path, active: false, block: false)
+    classes = []
+    classes << (active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white")
+    classes << (block ? "block" : "rounded-md")
+    classes << "px-3 py-2 text-sm font-medium"
+    link_to name, path, class: classes.join(" "), "aria-current": (active ? "page" : nil)
+  end
 end
