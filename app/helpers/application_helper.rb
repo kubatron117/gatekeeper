e9ff44087e647cd2368cca7b1fc418c24107209b
Kubatron 117 <kubatron117@gmail.com>
2025-06-21 14:00:01 +0200

    Add active for menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
-  def nav_link_to(name, path, active: false, block: false)
+  def nav_link_to(name, path, active: nil, block: false)
+    is_active = active.nil? ? current_page?(path) : active
+
     classes = []
-    classes << (active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white")
+    classes << (is_active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white")
     classes << (block ? "block" : "rounded-md")
     classes << "px-3 py-2 text-sm font-medium"
-    link_to name, path, class: classes.join(" "), "aria-current": (active ? "page" : nil)
+    link_to name, path, class: classes.join(" "), "aria-current": (is_active ? "page" : nil)
   end
 end
